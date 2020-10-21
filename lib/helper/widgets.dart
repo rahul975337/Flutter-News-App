5bed16e8acdebb15e9db54e4d84d6796bfa5f5e0
Sanskar Jain <69080556+sans-23@users.noreply.github.com>
2020-10-21 21:34:08 +0530

    UI updated
    Loading spinner of home screen is changed, reason: Category is not future thing hence should be displayed as soon as app launches, its app thing. News tile is wrapped inside card widget and elevation added to separate news from each other, also other small changes made in this news tile. MyAppBar is changed, drawer is added for future update here categories can be added aur link to about page or other stuff. Most important update as per me is categorycard it is much more visible and accessible, also looking far more decent, size of the container adjusted to look  way more elgant. Other logics like checking newslist if it is null before rendering listview.builder as this may show that red error screen if null returned in json response : Bad user experience right??.. import statement made a way more robust so other can collaborate with you if they are compelled to copy paste your code for some reason...

--- lib/helper/widgets.dart
+++ lib/helper/widgets.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:newsApp/views/article_view.dart';
+import '../views/article_view.dart';
 
-Widget MyAppBar(){
+Widget MyAppBar() {
   return AppBar(
     title: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         Text(
           "Flutter",
-          style:
-          TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
+          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
         ),
         Text(
           "News",
@@ -17,70 +16,98 @@ Widget MyAppBar(){
         )
       ],
     ),
-    backgroundColor: Colors.transparent,
-    elevation: 0.0,
+    backgroundColor: Colors.white,
+    elevation: 1.0,
+    actions: <Widget>[
+      Opacity(
+        opacity: 0.8,
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Icon(
+              Icons.share,
+            )),
+      )
+    ],
   );
 }
-
 
 class NewsTile extends StatelessWidget {
   final String imgUrl, title, desc, content, posturl;
 
-  NewsTile({this.imgUrl, this.desc, this.title, this.content, @required this.posturl});
+  NewsTile(
+      {this.imgUrl,
+      this.desc,
+      this.title,
+      this.content,
+      @required this.posturl});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(
-            builder: (context) => ArticleView(
-              postUrl: posturl,
-            )
-        ));
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ArticleView(
+                      postUrl: posturl,
+                    )));
       },
-      child: Container(
-          margin: EdgeInsets.only(bottom: 24),
-          width: MediaQuery.of(context).size.width,
-          child: Container(
+      child: Card(
+        elevation: 5,
+        child: Container(
+            margin: EdgeInsets.only(bottom: 24),
+            width: MediaQuery.of(context).size.width,
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              alignment: Alignment.bottomCenter,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(6),bottomLeft:  Radius.circular(6))
+              child: Container(
+                // padding: EdgeInsets.symmetric(horizontal: 16),
+                alignment: Alignment.bottomCenter,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(6),
+                        bottomLeft: Radius.circular(6))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: Image.network(
+                          imgUrl,
+                          height: 200,
+                          width: MediaQuery.of(context).size.width,
+                          fit: BoxFit.cover,
+                        )),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Container(
+                      color: Colors.grey[200],
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        title,
+                        maxLines: 2,
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        desc,
+                        maxLines: 2,
+                        style: TextStyle(color: Colors.black54, fontSize: 14),
+                      ),
+                    )
+                  ],
+                ),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ClipRRect(
-                      borderRadius: BorderRadius.circular(6),
-                      child: Image.network(
-                        imgUrl,
-                        height: 200,
-                        width: MediaQuery.of(context).size.width,
-                        fit: BoxFit.cover,
-                      )),
-                  SizedBox(height: 12,),
-                  Text(
-                    title,
-                    maxLines: 2,
-                    style: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    desc,
-                    maxLines: 2,
-                    style: TextStyle(color: Colors.black54, fontSize: 14),
-                  )
-                ],
-              ),
-            ),
-          )),
+            )),
+      ),
     );
   }
 }
